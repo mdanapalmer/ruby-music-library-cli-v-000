a41e731a60a087824d9a353507cb59f741bc9c38
Dana Palmer <roiai4@hotmail.com>
2018-11-07 04:32:02 +0000

    automatically backed up by learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -48,7 +48,8 @@ def self.find_by_name(name)
 end
 
 def self.find_or_create_by_name(name)
-  find_by_name(name) ||= song = Song.new(name)
+  find_by_name(name) ||= 
+  song = Song.new(name)
 end
 
 end
